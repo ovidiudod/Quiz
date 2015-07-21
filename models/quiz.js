fc13d8732e9cf58a7b0d349e5b4ba22ad568967f
ovidiudod <ovidiudod@gmail.com>
2015-07-21 22:49:04 +0200

    added database

---
+++ models/quiz.js
@@ -0,0 +1,15 @@
+module.exports = function (sequelize, DataTypes) {
+
+  var quizModel = {
+    pregunta  : {
+      type: DataTypes.STRING,
+      validate: { notEmpty: { msg: "-> Falta Pregunta" }}
+    },
+    respuesta : {
+      type: DataTypes.STRING,
+      validate: { notEmpty: { msg: "-> Falta Respuesta" }}
+    }
+  };
+
+  return sequelize.define('Quiz', quizModel);
+}
