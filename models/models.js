70686bbc8edfcf709cfb606372754ab2f2c5a516
ovidiudod <ovidiudod@users.noreply.github.com>
2015-07-26 22:53:41 +0200

    Update models.js

--- models/models.js
+++ models/models.js
@@ -38,8 +38,8 @@ sequelize.sync().then(function() {
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.bulkCreate( 
-        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
-          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma', tema:"ocio"},
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa', tema:"ciencia"}
         ]
       ).then(function(){console.log('Base de datos inicializada')});
     };
