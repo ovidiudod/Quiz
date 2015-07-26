3be486033d2af7bdae636d54486efb00338f7203
ovidiudod <ovidiudod@gmail.com>
2015-07-26 13:32:38 +0200

    Creación/Edición preguntas

--- models/quiz.js
+++ models/quiz.js
@@ -1,15 +1,14 @@
-module.exports = function (sequelize, DataTypes) {
+// Definicion del modelo de Quiz
 
-  var quizModel = {
-    pregunta  : {
-      type: DataTypes.STRING,
-      validate: { notEmpty: { msg: "-> Falta Pregunta" }}
-    },
-    respuesta : {
-      type: DataTypes.STRING,
-      validate: { notEmpty: { msg: "-> Falta Respuesta" }}
-    }
-  };
-
-  return sequelize.define('Quiz', quizModel);
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Quiz',
+            { pregunta: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Pregunta"}}
+      },
+              respuesta: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+	}
+            });
 }
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,26 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
+router.get('/search', function(req, res) {
+  res.render('search');
+});
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', 				   sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',              sessionController.loginRequired, multer({ dest: './public/media/'}), quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.ownershipRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.ownershipRequired, multer({ dest: './public/media/'}), quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.ownershipRequired, quizController.destroy);
 router.get('/quizes/author',   quizController.author);
-
-
-
 module.exports = router;
